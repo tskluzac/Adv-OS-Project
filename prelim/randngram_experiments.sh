80981516fcb055c27d71cee947090d5507d358c0
pub8 user <pub8User@pub8>
2017-12-10 12:37:48 -1000

    New README

--- prelim/randngram_experiments.sh
+++ prelim/randngram_experiments.sh
@@ -1,4 +1,4 @@
-python3 main.py /home/tskluzac/pub8/ svc randngram --out data/gt.csv --reader naivetruth.csv
+python3 main.py /home/tskluzac/pub8/ svc randngram --out data/gt.csv --reader naivetruth.csv # default n is 1
 python3 main.py /home/tskluzac/pub8/ logit randngram --out data/gt.csv --reader naivetruth.csv
 python3 main.py /home/tskluzac/pub8/ rf randngram --out data/gt.csv --reader naivetruth.csv
 
