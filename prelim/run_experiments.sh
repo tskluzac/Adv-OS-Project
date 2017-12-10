56c4b7ad57d3b847b7f37471b4780709742740eb
Goutham Rajendran <goutham@uchicago.edu>
2017-12-10 17:58:25 -0600

    Updated scripts

--- prelim/run_experiments.sh
+++ prelim/run_experiments.sh
@@ -18,7 +18,18 @@ python3 main.py /home/tskluzac/pub8/ rf rand --out data/gt.csv --reader naivetru
 python3 main.py /home/tskluzac/pub8/ rf randhead --head-bytes 256 --rand-bytes 256 --out data/gt.csv --reader naivetruth.csv
 """
 
-echo "Running jsd experiments with file data/extension"
+echo "Running jsd experiments 1/2 with file data/extension"
+python3 main.py /home/tskluzac/pub8/ jsd randngram --ngram 1 --rand-bytes 200 --out data/ext1gram200.csv
+echo "Running jsd experiments 2/2 with file data/extension"
+python3 main.py /home/tskluzac/pub8/ jsd randngram --ngram 2 --rand-bytes 200 --out data/ext2gram200.csv
+
+echo "Running jsd experiments 1/2 with file data/extension"
+python3 main.py /home/tskluzac/pub8/ jsd randngram --ngram 1 --rand-bytes 200 --out data/gt1gram200.csv --reader naivetruth.csv
+echo "Running jsd experiments 2/2 with file data/extension"
+python3 main.py /home/tskluzac/pub8/ jsd randngram --ngram 2 --rand-bytes 200 --out data/gt2gram200.csv --reader naivetruth.csv
+
+"""
+#echo "Running jsd experiments with file data/extension"
 python3 main.py /home/tskluzac/pub8/ jsd randngram --ngram 1 --rand-bytes 256 --out data/ext1gram256.csv
 python3 main.py /home/tskluzac/pub8/ jsd randngram --ngram 1 --rand-bytes 512 --out data/ext1gram512.csv
 python3 main.py /home/tskluzac/pub8/ jsd randngram --ngram 1 --rand-bytes 1024 --out data/ext1gram1024.csv
